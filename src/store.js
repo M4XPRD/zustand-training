cc3b0c2d361f299a80e109ab95a6f14ed557ae36
Max <ppmax@bk.ru>
2023-12-15 23:20:29 +0300

    Zustand store created

---
+++ src/store.js
@@ -0,0 +1,46 @@
+import { nanoid } from 'nanoid';
+import { create } from 'zustand';
+import {
+  devtools, persist, createJSONStorage,
+} from 'zustand/middleware';
+
+export const useTodos = create(
+  devtools(
+    persist(
+      (set, get) => ({
+        todos: [
+          { id: 1, title: 'Learn JS', completed: true },
+          { id: 2, title: 'Learn React', completed: false },
+        ],
+        loading: false,
+        error: null,
+        // addTodo: (title) => set((state) => {
+        //   const newTodo = { id: nanoid(), title, completed: false };
+        //   return { todos: [...state.todos, newTodo] };
+        // }),
+        // addTodo: (title) => set((state) => ({
+        //   todos: [...state.todos, { id: nanoid(), title, completed: false }],
+        // })),
+        addTodo: (title) => {
+          const newTodo = { id: nanoid(), title, completed: false };
+          set({ todos: [...get().todos, newTodo] });
+        },
+        toggleTodo: (todoId) => set({
+          todos: get().todos
+            .map((todo) => (todoId === todo.id ? { ...todo, completed: !todo.completed } : todo)),
+        }),
+      }),
+      {
+        name: 'todos-storage',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+  ),
+);
+
+export const useFilter = create(
+  devtools((set) => ({
+    filter: 'all',
+    setFilter: (value) => set({ filter: value }),
+  })),
+);
