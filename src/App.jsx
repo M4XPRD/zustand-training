69f8938a8e6f021dbfe1c601f743bc9ff31df51b
Max <ppmax@bk.ru>
2023-12-15 23:21:04 +0300

    Main component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { VStack, Divider } from '@chakra-ui/react';
+import Filter from './components/Filter';
+import TodoList from './components/TodoList';
+import TotalTodos from './components/TotalTodos';
+import NewTodo from './components/NewTodo';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <VStack spacing={4}>
+    <Filter />
+    <TodoList />
+    <Divider />
+    <TotalTodos />
+    <NewTodo />
+  </VStack>
+);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
